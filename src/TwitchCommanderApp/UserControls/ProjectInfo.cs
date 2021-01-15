a7c1ac10d0fb7d138de93067db93c80942fcc34d
Chad Green <chad.green@talelearncode.com>
2021-01-15 11:02:03 -0500

    Adding user controls from laptop

---
+++ src/TwitchCommanderApp/UserControls/ProjectInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaleLearnCode.TwitchCommander.UserControls
+{
+	public partial class ProjectInfo : UserControl
+	{
+		public ProjectInfo()
+		{
+			InitializeComponent();
+		}
+	}
+}
